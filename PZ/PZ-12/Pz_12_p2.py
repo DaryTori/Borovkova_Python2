cf974bcec3772c0222cd6cf7318712c402a39322
DaryTori <borovkova.daria7@gmail.com>
2025-05-10 12:20:10 +0300

    мелочь

--- PZ/PZ-12/Pz_12_p2.py
+++ PZ/PZ-12/Pz_12_p2.py
@@ -1,5 +1,5 @@
 #Составить генератор (yield), который выводит из строки только цифры.
 stroka = input("Введите строку: ")
-def cifri(s): 
-    yield from [n for n in s if n.isdigit()]
+def cifri(st): 
+    yield from [n for n in st if n.isdigit()]
 print(*list(cifri(stroka)))
