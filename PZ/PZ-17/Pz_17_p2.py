08e61806fccd91cac725d797ff00029c554b1457
DaryTori <borovkova.daria7@gmail.com>
2025-06-13 13:19:30 +0300

    17-2

--- PZ/PZ-17/Pz_17_p2.py
+++ PZ/PZ-17/Pz_17_p2.py
@@ -4,6 +4,20 @@
 import tkinter as tk
 from tkinter import ttk
 
+def f(s):
+    try: return "Число является четным" if int(s) % 2 == 0 else "Число не является четным"
+    except: return "NOT INT"
+
 root = tk.Tk()
 root.title("Проверка чётности")
+root.geometry("500x300")
 
+s = tk.StringVar()
+
+entry = tk.Entry(root)
+btn = tk.Button(root, text="Click", command=lambda: s.set(f(entry.get())))
+lbl = tk.Label(root, textvariable=s)
+entry.pack()
+btn.pack()
+lbl.pack()
+root.mainloop()
